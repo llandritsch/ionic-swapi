25a660f4b00f4b3edef801849af27fd8003deba9
Andritsch, Lisa <landritsch@madisoncollege.edu>
2021-05-06 15:51:49 -0500

    rxjs demo with api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwapiService } from '../swapi.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private swapi: SwapiService) {}
 
+  planets = [];
+
+  ngOnInit() {
+    this.swapi.loadPlanets().subscribe(
+      data => {
+        console.log(data);
+        this.planets = [
+          ...this.planets
+          , ...(data as any).results
+        ].sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase()? 1 : -1);
+      }
+      , err => console.error(err)
+    );
+
+  }
 }
--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { merge, race, EMPTY } from 'rxjs';
+import { expand, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadPlanets() {
+    //return this.http.get("https://swapi.dev/api/planets/");
+    const pageOne = this.http.get("https://swapi.dev/api/planets/");
+    //const pageTwo = this.http.get("https://swapi.ev/api/planets/?page=2");
+
+//return race(
+ //     pageOne,
+ //     pageTwo
+ //   );
+
+    return pageOne.pipe(
+      tap(x => console.log(x)),
+      expand(
+        x => (x as any).next ? this.http.get((x as any).next) : EMPTY
+      )
+    )
+  }
 }
